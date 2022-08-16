98194eee40245f2ed6271ab59e9affff2d424014
孙剑桥 <413326498@qq.com>
2022-08-16 14:08:53 +0800

    第二部分：Model And TableView

--- src/main/java/sample/control/RootLayoutController.java
+++ src/main/java/sample/control/RootLayoutController.java
@@ -10,30 +10,6 @@ import javafx.scene.text.Text;
 public class RootLayoutController {
 
     @FXML
-    private TextField username;
-    @FXML
-    private PasswordField password;
-    @FXML
-    private Button login;
-    @FXML
-    private Text loginmessage;
-
-    private String usernameString;
-    private String passwordString;
-
-    @FXML
     public void initialize(){
-    }
-    @FXML
-    public void login(ActionEvent event){
-        System.out.println("Controller.login" + "----event.getEventType() 值= " + event.getEventType());
-        usernameString=username.getText();
-        passwordString=password.getText();
-
-        System.out.println("Controller.initialize" + "----usernameString 值= " + usernameString);
-        System.out.println("Controller.initialize" + "----passwordString 值= " + passwordString);
-
-        loginmessage.setText("正在登录，请稍后");
-        //正在登录中，进行登录验证和跳转操作
     }
 }
